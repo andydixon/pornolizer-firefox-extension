5cfaecf6f4c357d14c7514c2d62002346c8b55b4
Andy Dixon <andy@andydixon.com>
2025-01-27 21:36:28 +0000

    Initial

---
+++ content-script.js
@@ -0,0 +1,249 @@
+const PORN_DATA = {
+  names: {
+    en: [
+      "Big Cock", "Fuck me for a Buck", "Dirk Diggler", "Big Dick", "Give it to me", "Cock Sucker", "Up the Arse",
+      "Cockboy", "Omar Pussy", "Fat Ass", "Superdick", "Mistress Shiva", "Bite Me", "Mistress Anal", "Long Finger", "Plugin", "Anal",
+      "Ass-stitcher", "Jar Jar", "Bust-a-Cunt", "Buzzwordbaby", "Sniff-my-Ass", "Dripper Dick", "Butplug", "The-Champ", "Fill me up",
+      "Suck my tits dry", "Ball Buster", "Dildo", "Motherfucker", "Dickwad", "Fuckface", "Jerkoff", "Bitch", "Bastard", "Asshole",
+      "Hard-on", "Pimp Mastah", "Son of a whore", "Muffdiver", "Rugmuncher", "Admiral Browning", "Afterburner", "Airing the Orchid",
+      "Aphrodite's Evostick", "Ballbuffer", "Muffmuncher", "Cuntcleaner", "Mount Thrushmore", "Scrotscrubber", "Muffminer", "Anusapple",
+      "Mouth-full-o'-cock", "Nobgoblin", "Fannyfarmer", "Spunksupper", "Clitcollector", "Bumbanger", "Assrush", "Bonebagger", "Saggysack",
+      "Cumbucket", "King Choad"
+    ],
+    de: [
+      "Popopirat", "Samenstau", "Lutsch du Luder", "Rosettenslasher", "Wundfick", "Fickapparat", "Analsonde", "Spermagarage", "Eiterfotze",
+      "Flatterfotze", "Resteficker", "Analraupe", "Spermagurgler", "Analprinz", "Analkanal", "Analhölenforscher", "Schimmelpimmel", "Schwanzpastete",
+      "Prärieficker", "Analspecht", "Rosettentester", "Vorbläser", "Hühnerficker", "Ziegenficker", "Analgeneral", "Fickmonster", "Schluck du Luder",
+      "Rosettenrohrkrepierer", "Rustikalfick", "Hodensauger", "Samenstaumechaniker", "Popostecher", "Steckdosenbefruchter", "Analtorpedo",
+      "Spermarutsche", "Fotzenkäse", "Hodenkobolt", "Analfrosch", "Wichsfresse", "Penisgesicht", "Fickfetzen", "Affenschwanz", "Arschficker",
+      "Wichser", "Puffmutter", "Drecksau", "Arschgeiger", "Hurensohn", "Blas mir einen", "Dunkelbumser", "Ficksau", "Kekserlwichser", "Eisenbanger",
+      "Fleischknüppel", "Hodenvernichter", "Fotze", "Fotzenknecht", "Fotzendrescher", "Analfeuerwerk", "Leichenfick", "Sackgelutsche", "Analritter",
+      "Analpirat", "Kanalonanierer", "Schnalzhengst", "Elefantenprügel", "Ochsenpimmel", "Vaginalmassage", "Geisterscheisse", "Zieh an meinen Eiern",
+      "Massier mir die Rosette", "Rosette", "Saugsklave", "Drecksfotze", "Hängepimmel"
+    ],
+    dk: [
+      "Cocksucker", "Asslicker", "Horselover", "Horse Dick", "Knepp din mor", "Sut min pik", "Fat Ass", "Fistfucker", "Røvpuler", "Dominatrix",
+      "Penetrator", "Fuck me for a Buck", "Dirk Diggler", "Big Dick", "Give it to me", "Fittesnik", "Cock Sucker", "Up the Arse", "Cockboy",
+      "Omar Pussy", "Fat Ass", "Wide Canyon", "Superdick", "Bed Buster", "Mistress Shiva", "Bite Me", "Mistress Anal", "Anal Kanal ", "Long Finger",
+      "Plugin", "Anal", "Ass-stitcher", "Jar Jar", "Bust-a-Cunt", "Slingre Fitte", "Wet Sex", "Buzzwordbaby", "Sniff-my-Ass", "Dripper Dick", "Butplug",
+      "The-Champ", "Fill me up", "Suck my tits dry"
+    ],
+    es: [
+      "culo de gordo de", "chocho de caliente de", "hijo de puta de", "chupa de pollas de", "come de mierda de"
+    ],
+    hr: [
+      "dugi", "jebozov", "kucka", "koji kurac", "kurva", "picka", "bradavica", "bradavice k'o karfioli", "guzonja", "jebo sliku svoju", "dupe",
+      "cmar", "prstenac", "pickin dim", "picopevac", "koji kurac palac", "jebo mamu", "jebo sestru", "jebo jeza", "iskusni"
+    ],
+    hu: [
+      "Nagy Fasz", "Kúrd A Seggem", "Faszdörgölõ", "Pöcsmag", "Ánuszszaggató", "Kékeres Méteres", "Seggdugasz", "Csirkebaszó", "Pöttöm Kuki",
+      "Fideszközeli", "Pöcshuszár", "Puncivitéz", "Rongyosra Baszlak", "Huncut A Seggem", "Õsparaszt", "Családunk Hímtagja", "Tedd A Seggembe",
+      "Vadvalagú", "Hájas Valag", "Harapj Belém", "Hervadt Fasz", "Rojtos Puncus", "Puncimókus", "A Hosszú", "Csöpög A Pöcsöm", "Dikkmá",
+      "Királyi Fallosz", "Buzinak Születtem", "Baszomalássan", "Xavér", "Murváspöcsû", "Züllött Farokverõ", "Csiklókandúr", "Égszinkék Heregolyó",
+      "A Legkissebb Ugrifüles", "Faszverõ Szamuráj", "Démoni Faszharcos", "Pernahajder Campbell", "Gézengúz Recskabajnok", "Kinyírom Mindet!",
+      "Masszív Fasz", "Gazsi", "Oktondi", "Punnyadt Punci"
+    ],
+    no: [
+      "Big Cock", "Fuck me for a Buck", "Dirk Diggler", "Big Dick", "Give it to me", "Fittesnik", "Cock Sucker", "Up the Arse", "Mr. Bolt i tvåan",
+      "Smellan i tvåan", "Cockboy", "Omar Pussy", "Fat Ass", "Superdick", "Mistress Shiva", "Bite Me", "Mistress Anal", "Anal Kanal", "Romperæseren",
+      "Long Finger", "Plugin", "Anal", "Ass-stitcher", "Jar Jar", "Bust-a-Cunt", "Slingre Fitte", "Buzzwordbaby", "Hengepikk", "Sniff-my-Ass", "Luremus",
+      "Dripper Dick", "Slurpe Fitte", "Butplug", "Fjellkuk", "The-Champ", "Fill me up", "Suck my tits dry", "Tarmslyng", "Tarmen elektra", "Fluffy pung",
+      "Flyffy pikk", "Bygdehomo", "Slingrefitte", "Lille gutt", "Gamle Erik", "Stål ånde", "Fantomkuk", "Spybøtte", "Råtefitte", "Stålfitte", "Hengemus",
+      "Fitteleppe", "Hengeballe", "Stålkuk", "Hårball", "Pelsdott", "Kukhode", "Hengepupp", "Svulmemus", "Trekuk", "Kukskalle", "Panserpung", "Kukost",
+      "Up the bum"
+    ],
+    se: [
+      "Big Cock", "Fuck me for a Buck", "Dirk Diggler", "Big Dick", "Give it to me", "Fittesnik", "Cock Sucker", "Up the Arse",
+      "Mr. Bolt i tvåan", "Smellan i tvåan", "Cockboy", "Omar Pussy", "Fat Ass", "Superdick", "Mistress Shiva", "Bite Me", "Mistress Anal",
+      "Anal Kanal ", "Romperæseren", "Long Finger", "Plugin", "Anal", "Ass-stitcher", "Jar Jar", "Bust-a-Cunt", "Slingre Fitte", "Buzzwordbaby",
+      "Hengepikk", "Sniff-my-Ass", "Luremus", "Dripper Dick", "Slurpe Fitte", "Butplug", "Fjellkuk", "The-Champ", "Fill me up", "Suck my tits dry",
+      "Åderpåle", "Balle", "Åka skinnhiss", "Gå ut med hunden", "Hälsa på monstret", "Lira skinnflöjt", "Smörja spettet", "Fisken", "Jacket",
+      "Springan", "Spettar", "Fuckar", "Bakis-knull", "Fylle-juck", "Drägg-knull"
+    ]
+  },
+
+  prose: {
+    en: [
+      "fuck", "smooch", "smack", "peck", "unclefuck", "spank", "motherfuck", "deep throat", "ballbust", "spew", "dripp",
+      "thrust", "cocksuck", "fistfuck", "fist", "suck", "squirt", "wank", "bang", "brown", "cuntlick", "cuntlapp", "felch",
+      "fingerfuck", "gangbang", "plow", "raunch", "screw", "sex fight", "titty fuck", "enter", "raid", "jerk", "finger",
+      "shaft", "blow", "lick", "asslick", "fuck", "assfuck", "wad pull", "muff sniff", "aardvark", "ball", "barf", "charver",
+      "cream", "fart", "fomp", "gamahuche"
+    ],
+    de: [
+      "fotzenleck", "arschbums", "wixefress", "niedernudeln", "herknecht", "drüberbürst", "niederknall", "penislutsch",
+      "pimmelsaug", "niederschnalz", "fleischlutsch", "analpenetrier", "rosettenreit", "schwanzlutsch", "bums", "fick",
+      "saug", "schleck", "arschfick", "fotzereib", "wichs", "muttifick", "analsaug", "masturbier", "faustfick", "schleimscheiß",
+      "schwanzsaug", "schwanzfick", "pimmelsaug", "tittensaug", "flöt", "möpsesaug"
+    ],
+    dk: [
+      "pul", "knepp", "voldtag", "boll", "spank", "røvpul", "fistfuck", "prygl", "riv", "sut", "rid", "masturber", "onaner", "voldknepp", "fuck", "suck"
+    ],
+    es: [
+      "foll", "dilat", "chup", "encul", "insult", "cag", "me", "eyacul", "pute", "fornic", "viol", "escup"
+    ],
+    hr: [
+      "jeb-anje-ao-ala-ite", "liz-anje-ao-ala-ite", "kar-anje-ao-ala-ajte", "tuc-anje-ao-ala-ajte", "trp-anje-ao-ala-ajte",
+      "kres-anje-ao-ala-ite", "pis-anje-ao-ala-ajte", "svrsav-anje-ao-ala-ajte", "dudl-anje-ao-ala-ajte", "pus-enje-io-ila-ite",
+      "guz-enje-io-ila-ite", "bar-enje-io-ila-ite", "gaz-enje-io-ila-ite"
+    ],
+    hu: [
+      "buziz", "felbasz", "geciz", "szop", "beszív", "beszop", "anyáz", "szopat", "szar", "fing", "kibasz", "benyal", "elfaszoskod",
+      "pisál", "lekurváz", "szájbabasz", "genyáz", "mókáz", "fos", "recskáz", "szétbasz", "majomkod", "baromkod", "szomorkod", "kifing",
+      "izmoz", "kukiz", "fikáz", "masztiz", "turház", "bunkóz"
+    ],
+    no: [
+      "pul", "slikk", "knull", "rævkjør", "skamknull", "runk", "spank", "sug", "høvl", "bakpul", "fist", "møljejukk", "tvangspul",
+      "drit", "jukk", "grafs", "nevepul", "fittegnukk", "kuksprut", "forderv", "svimepul", "masturber", "kukkspreng", "sædslikk",
+      "gjengpul", "sprengpul", "fittefyll", "grøfteknull", "fittepryl", "flaskeknull", "fittepryl", "svelg", "svett", "rævslikk",
+      "romperes", "tarmfres", "tafs", "likpul", "blodsug", "kuksug"
+    ],
+    se: [
+      "knull", "slick", "rövknull", "skamknull", "runk", "spank", "fist", "tvångsknull", "bajs", "juck", "tafs", "pull", "fittgnid",
+      "kuksprut", "simknull", "masturber", "dumkåt", "svälj", "gangbang", "dragg", "flaskknull", "åderpål", "spett", "fuck", "bakis-knull",
+      "fylle-juck", "drägg-knull"
+    ]
+  }
+};
+
+/**********************************************
+ * UTILITY: Titlecase checker
+ **********************************************/
+function isTitleCase(word) {
+  if (!word) return false;
+  // Check if first char uppercase and the rest lowercase.
+  // E.g. "Hello" => first letter is "H" uppercase, "ello" is all lower.
+  const first = word.charAt(0);
+  const rest = word.slice(1);
+  return first === first.toUpperCase() && rest === rest.toLowerCase();
+}
+
+/**********************************************
+ * NAME CONVERTER 
+ **********************************************/
+function nameConverter(text, language, swearibility) {
+  // If language not present, just return original text
+  if (!PORN_DATA.names[language]) {
+    return text;
+  }
+
+  const nameArray = PORN_DATA.names[language];
+  const words = text.split(" ");
+  let hasMatch = 0;
+  let returnString = "";
+
+  for (let i = 0; i < words.length; i++) {
+    const w = words[i];
+
+    if (isTitleCase(w)) {
+      hasMatch++;
+      // When hasMatch = 2, we reset it and possibly inject a random name
+      if (hasMatch === 2) {
+        hasMatch = 0;
+        // random chance
+        const randNum = Math.floor(Math.random() * 100);
+        if (randNum < (swearibility - 1)) {
+          // pick random name from nameArray
+          const selectedName = nameArray[Math.floor(Math.random() * nameArray.length)];
+          returnString += `"${selectedName}" `; 
+        }
+      }
+    } else {
+      hasMatch = 0;
+    }
+
+    // Always add the original word with a trailing space
+    returnString += w + " ";
+  }
+
+  return returnString;
+}
+
+/**********************************************
+ * PROSE CONVERTER 
+ **********************************************/
+function proseConverter(text, language, swearibility) {
+  // If language not present, print or return the same text
+  if (!PORN_DATA.prose[language]) {
+    console.warn("Unsupported Language: " + language);
+    return text;
+  }
+
+  const proseArray = PORN_DATA.prose[language];
+  const words = text.split(" ");
+  let returnString = "";
+
+  for (let i = 0; i < words.length; i++) {
+    let w = words[i];
+    let suffix = "";
+
+    // Check for typical English suffixes: ing, ed, s
+    if (w.endsWith("ing")) {
+      suffix = "ing";
+    } else if (w.endsWith("ed")) {
+      suffix = "ed";
+    } else if (w.endsWith("s")) {
+      suffix = "s";
+    }
+
+    // Random chance to replace
+    if (suffix && Math.floor(Math.random() * 100) < (swearibility - 1)) {
+      // pick random from prose
+      const randomProse = proseArray[Math.floor(Math.random() * proseArray.length)];
+      w = randomProse + suffix;
+    }
+
+    returnString += w + " ";
+  }
+
+  return returnString;
+}
+
+function pornolize(text, language, swearibility) {
+  // 1) Convert capitalized words to insert random names
+  let withNames = nameConverter(text, language, swearibility);
+  // 2) Convert words ending with "ing"/"ed"/"s" to a random vulgar/prose
+  let finalText = proseConverter(withNames, language, swearibility);
+  return finalText;
+}
+
+function transformText(originalText) {
+  return pornolize(originalText,'en',80);
+}
+
+/**
+ * Recursively walk the DOM, looking for text nodes,
+ * and apply a handler function to each text node.
+ */
+function walk(node) {
+  let child, next;
+
+  switch (node.nodeType) {
+    case 1: // Element
+    case 9: // Document
+    case 11: // Document fragment
+      child = node.firstChild;
+      while (child) {
+        next = child.nextSibling;
+        walk(child);
+        child = next;
+      }
+      break;
+    case 3: // Text node
+      handleText(node);
+      break;
+  }
+}
+
+/**
+ * Handle a text node by transforming its text content
+ * and overwriting the nodeValue with the result.
+ */
+function handleText(textNode) {
+  const originalText = textNode.nodeValue;
+  const newText = transformText(originalText);
+  textNode.nodeValue = newText;
+}
+
+// Start walking from the document's body
+walk(document.body);
